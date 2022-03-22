0bdf16e3616fc644ee37e7e1d602af631e0d2ac4
Khayman King <khaymanak@gmail.com>
2022-03-22 11:15:06 -0700

    added fetch-utils

---
+++ fetch-utils.js
@@ -0,0 +1,7 @@
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlvZ3JmY3dnbHduaXN3dWlsZnN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDc1NTM0MDAsImV4cCI6MTk2MzEyOTQwMH0._gHMV0sj3d-0HPIgU3x1yZVFLsxu68U2NdqEm0LxnFg';
+
+
+const SUPABASE_URL = "https://iogrfcwglwniswuilfsw.supabase.co"
+
+const client = createClient(SUPABASE_URL, process.env.SUPABASE_KEY);
+
