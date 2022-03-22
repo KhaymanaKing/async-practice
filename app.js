f5ad41160dcd6f2497fd23bf0cadbbaba81f8714
Khayman King <khaymanak@gmail.com>
2022-03-22 11:24:20 -0700

    added fetch bears to app.js

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 // import functions and grab DOM elements
-
+import { getBears } from './fetch-utils.js';
 // let state
 
 // set event listeners 
+
+async function fetchAndDisplayBears(){
+
+    const bears = await getBears();
+    for (let bear of bears) {
+
+        const bearEl = renderBear(bear);
+        bearListEl.append(bearEl);
+    }
+}
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
